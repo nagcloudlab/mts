776a6475337aaae23258ed76e804ecaf1000de8d
Dev-1 <dev1@example.com>
2026-08-20 14:39:34 +0530

    mts backend intial project

---
+++ mts-backend/src/main/java/com/example/MtsBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsBackendApplication.class, args);
+
+		int a = 10;
+	}
+
+}
